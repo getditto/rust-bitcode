d2e6dcd2d3440901896bd4516837eebc8233b9f8
Thomas Karpiniec <tom@ditto.live>
2020-01-10 12:34:35 +1100

    Update to nightly-2019-12-16

--- config.sh
+++ config.sh
@@ -1,16 +1,17 @@
 # 1. Select the best branch from https://github.com/apple/swift-llvm
 
 # $ xcrun -sdk iphoneos swiftc --version
-# Apple Swift version 5.0.1 (swiftlang-1001.0.82.4 clang-1001.0.46.5)
-# Target: x86_64-apple-darwin18.7.0
+# Apple Swift version 5.1 (swiftlang-1100.0.270.13 clang-1100.0.33.7)
+# Target: x86_64-apple-darwin19.0.0
 
-# So this branch seems a likely candidate
-SWIFT_BRANCH="swift-5.0-branch"
+# There is a branch called "stable" which looks promising
+# At this time (running Xcode 11.3) it's a couple of months newer than the 5.1 branch
+SWIFT_BRANCH="stable"
 
 # 2. Pick/install a working Rust nightly (ideally one where RLS and clippy built)
 # 3. Note its date
-RUST_NIGHTLY="2019-09-05"
+RUST_NIGHTLY="2019-12-16"
 
 # 4. Get its commit - this is what we will check out to build the iOS version
 # rustc --version | cut -d '(' -f2 | cut -d ' ' -f1
-RUST_COMMIT="c6e9c76c5"
+RUST_COMMIT="a605441e0"
