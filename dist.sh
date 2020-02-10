08d10ec9a0da95960e7f6ce513aae353f4314a0b
Pruthvikar Reddy <pruthvikar@gmail.com>
2020-02-10 10:11:46 +0000

    Updated variables in dist.sh

--- dist.sh
+++ dist.sh
@@ -3,8 +3,8 @@ set -euxo
 source config.sh
 
 WORKING_DIR="$(pwd)/build"
-DEST="$(pwd)/dist/rust-ios-arm64-${RUST_NIGHTLY}"
-TOOLCHAIN_DEST="${DEST}/toolchain-${RUST_NIGHTLY}"
+DEST="$(pwd)/dist/rust-ios-arm64-${RUST_TOOLCHAIN}"
+TOOLCHAIN_DEST="${DEST}/toolchain-${RUST_TOOLCHAIN}"
 
 rm -rf "$TOOLCHAIN_DEST"
 mkdir -p "$TOOLCHAIN_DEST"
@@ -14,12 +14,12 @@ cp LICENSE* README.md "$DEST"
 
 rm -rf "$DEST/install.sh"
 echo "#!/bin/bash" >> "$DEST/install.sh"
-echo "DEST_TOOLCHAIN=\"\$HOME/.rust-ios-arm64/toolchain-$RUST_NIGHTLY\"" >> "$DEST/install.sh"
+echo "DEST_TOOLCHAIN=\"\$HOME/.rust-ios-arm64/toolchain-$RUST_TOOLCHAIN\"" >> "$DEST/install.sh"
 echo "mkdir -p \"\$DEST_TOOLCHAIN\"" >> $DEST/install.sh
-echo "cp -r \"toolchain-$RUST_NIGHTLY\"/* \"\$DEST_TOOLCHAIN\"" >> "$DEST/install.sh"
+echo "cp -r \"toolchain-$RUST_TOOLCHAIN\"/* \"\$DEST_TOOLCHAIN\"" >> "$DEST/install.sh"
 echo "rustup toolchain link ios-arm64 \"\$DEST_TOOLCHAIN\"" >> "$DEST/install.sh"
 chmod +x "$DEST/install.sh"
 
 cd dist
-zip -r "rust-ios-arm64-${RUST_NIGHTLY}.zip" "rust-ios-arm64-${RUST_NIGHTLY}"
+zip -r "rust-ios-arm64-${RUST_TOOLCHAIN}.zip" "rust-ios-arm64-${RUST_TOOLCHAIN}"
 cd ..
