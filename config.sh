89a7c5764bd037652ae442a3e3aa81ba0fb606a8
Søren Mortensen <soren@neros.dev>
2021-01-28 00:29:23 +0000

    Update from Rust 1.46 to nightly-2021-01-25

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 # returned by the command below:
 # $ xcrun -sdk iphoneos swiftc --version
 
-LLVM_BRANCH="tags/swift-5.3-RELEASE"
+LLVM_BRANCH="tags/swift-5.3.2-RELEASE"
 
 # 2. Select the best branch, tag or commit hash from https://github.com/rust-lang/rust
 
-RUST_BRANCH="tags/1.46.0"
+RUST_BRANCH="1d0d76f8dd4f5f6ecbeab575b87edaf1c9f56bb8" # nightly-2021-01-25
 
 # 3. Select a name for the toolchain you want to install as. The toolchain will be installed
 # under $HOME/.rustup/toolchains/rust-$RUST_TOOLCHAIN
 
-RUST_TOOLCHAIN="ios-arm64-1.46.0"
+RUST_TOOLCHAIN="ios-arm64-nightly-2021-01-25"
 
